7769ed03bfea356a8c28799bb4f76e673fd6b138
BatuhanKIRICI <k.sukr48@gmail.com>
2023-05-31 20:45:51 +0300

    props usage of  and {}

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
-/* import React from "react";
-import ReactDOM from "react-dom/client";
-import HemisphereDisplay from "./HemisphereDisplay";
-
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
-  return (
-    <div>
-      <HemisphereDisplay />
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 import React from "react";
-import ReactDOM from "react-dom/client"; */
+import ReactDOM from "react-dom/client";
 
 const App = () => {
-  const nameOfMine = "Batuhan";
+  const type = "number";
+  const minVal = 5;
+  const maxVal = 10;
 
-  return (
-    <div>
-      My name is:
-      <h1>{nameOfMine}</h1>
-    </div>
-  );
+  return <input type="number" min={minVal} max={maxVal} />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
