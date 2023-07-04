59c45d2492d23b5aad21e827c76734c07522397c
BatuhanKIRICI <k.sukr48@gmail.com>
2023-07-04 17:46:14 +0300

    Personal digital asistants' cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <button>Get Advice</button>
+      <section className="hero is-small is-link">
+        <div className="hero-body">
+          <p
+            className="title"
+            style={{ textAlign: "center", fontSize: "3rem" }}
+          >
+            Personal Digital Asistants
+          </p>
+        </div>
+      </section>
+      <div
+        className="columns"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "30px",
+        }}
+      >
+        <div className="column is-one-fifth">
+          <ProfileCard
+            title="Alexa"
+            handle="@alexa99"
+            image={AlexaImage}
+            description="Alexa was created by Amazon."
+          />
+        </div>
+        <div className="column is-one-fifth">
+          <ProfileCard
+            title="Cortana"
+            handle="@cortana32"
+            image={CortanaImage}
+            description="Cortana is made by Microsoft."
+          />
+        </div>
+        <div className="column is-one-fifth">
+          <ProfileCard
+            title="Siri"
+            handle="@siri01"
+            image={SiriImage}
+            description="Siri belongs to Apple."
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,17 +1,21 @@
-function ProfileCard({ title, handle, image, description }) {
+function ProfileCard(props) {
+  const { title, handle, image, description } = props;
+
   return (
     <div className="card">
       <div className="card-image">
-        <figure class="image is-1by1">
-          <img src={image} alt="pda logo" />
+        <figure className="image is-1by1">
+          <img src={image} alt="Logos of pda" />
         </figure>
       </div>
       <div className="card-content">
-        <div className="media-content">
-          <p className="title is-4">Title is {title}</p>
-          <p className="subtitle is-6">Handle is {handle}</p>
+        <div className="media-content" style={{ textAlign: "center" }}>
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
         </div>
-        <div className="subtitle is-6">{description}</div>
+        <div className="content" style={{ padding: "20px" }}>
+          {description}
+        </div>
       </div>
     </div>
   );
