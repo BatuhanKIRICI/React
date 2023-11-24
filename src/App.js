9a249e2ad782c9eb1e5d9c12c55f77369bdb4e56
BatuhanKIRICI <k.sukr48@gmail.com>
2023-11-25 00:36:14 +0300

    "Changing colours with useState"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  const [palette, setPalette] = useState("gray");
+
+  const handleColor = () => {
+    setPalette((p) =>
+      p === "gray" || p === "red" ? (p = "green") : (p = "red")
+    );
+  };
+
+  return (
+    <div>
+      <div
+        onClick={handleColor}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          justifyContent: "center",
+          textAlign: "center",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: palette,
+          color: "white",
+          width: "450px",
+          height: "250px",
+          fontSize: "50px",
+          fontFamily: "Arial",
+          border: "15px solid black",
+          borderRadius: "15px",
+        }}
+      ></div>
+    </div>
+  );
 }
 
 export default App;
