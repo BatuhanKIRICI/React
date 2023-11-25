e49bc6f5b1ccc0c05bb20f9a97905a76f4daf52e
BatuhanKIRICI <k.sukr48@gmail.com>
2023-11-25 23:33:21 +0300

    "Bulma Card Example"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import Counter from "./app/features/counter/Counter";
-function App() {
+import "bulma/css/bulma.css";
+import Course from "./Course";
+import Picture from "./images/501757-The-Dark-Knight-Heath-Ledger-movies-quote-anime.jpg";
+import Image from "./images/Black-Joker-Wallpapers-HD.jpg";
+import Photo from "./images/joker.jpg";
+import Logo from "./images/joker2.jpg";
+
+const App = () => {
   return (
     <div>
-      <Counter />
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">The Joker</p>
+        </div>
+      </section>
+      <div className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <Course
+                title="Picture"
+                image={Picture}
+                description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia a, recusandae quo explicabo dolores aspernatur magnam beatae assumenda nihil quos saepe amet voluptate maxime voluptatem laboriosam cupiditate dignissimos quam libero."
+              />
+            </div>
+            <div className="column">
+              <Course
+                title="Image"
+                image={Image}
+                description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia a, recusandae quo explicabo dolores aspernatur magnam beatae assumenda nihil quos saepe amet voluptate maxime voluptatem laboriosam cupiditate dignissimos quam libero."
+              />
+            </div>
+            <div className="column">
+              <Course
+                title="Photo"
+                image={Photo}
+                description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia a, recusandae quo explicabo dolores aspernatur magnam beatae assumenda nihil quos saepe amet voluptate maxime voluptatem laboriosam cupiditate dignissimos quam libero."
+              />
+            </div>
+            <div className="column">
+              <Course
+                title="Logo"
+                image={Logo}
+                description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia a, recusandae quo explicabo dolores aspernatur magnam beatae assumenda nihil quos saepe amet voluptate maxime voluptatem laboriosam cupiditate dignissimos quam libero."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
