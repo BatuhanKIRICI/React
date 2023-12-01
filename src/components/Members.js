6033a4ad9c5e21cc2c0990a783ff847f8f9bb8cb
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-01 20:55:10 +0300

    "useSearchParams()"

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Members() {
-  return <div>Members</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get("filter") === "active";
+
+  return (
+    <>
+      <div>Members</div>
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Active Members
+      </button>
+      <button onClick={() => setSearchParams()}>All Members</button>
+      {isActive ? <h2>Active Members</h2> : <h2>All Members</h2>}
+    </>
+  );
 }
 
 export default Members;
