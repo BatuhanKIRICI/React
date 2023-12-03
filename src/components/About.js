099909e657dd8405ada853615a10e23f8a5934ec
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-03 16:43:45 +0300

    "About"

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,39 @@
 import React from "react";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../styles/about.css";
 
 function About() {
-  return <div>Hakkımızda</div>;
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+          architecto, fuga ipsum cum nemo quas iusto, debitis minima ratione,
+          sapiente vel libero aliquam quos quisquam odit necessitatibus delectus
+          voluptatibus exercitationem.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Officia architecto, fuga ipsum cum nemo quas iusto,
+          debitis minima ratione, sapiente vel libero aliquam quos quisquam odit
+          necessitatibus delectus voluptatibus exercitationem.Lorem ipsum dolor
+          sit amet consectetur, adipisicing elit. Officia architecto, fuga ipsum
+          cum nemo quas iusto, debitis minima ratione, sapiente vel libero
+          aliquam quos quisquam odit necessitatibus delectus voluptatibus
+          exercitationem.Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Officia architecto, fuga ipsum cum nemo quas iusto, debitis
+          minima ratione, sapiente vel libero aliquam quos quisquam odit
+          necessitatibus delectus voluptatibus exercitationem.Lorem ipsum dolor
+          sit amet consectetur, adipisicing elit. Officia architecto, fuga ipsum
+          cum nemo quas iusto, debitis minima ratione, sapiente vel libero
+          aliquam quos quisquam odit necessitatibus delectus voluptatibus
+          exercitationem.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
