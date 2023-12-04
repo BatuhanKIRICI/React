e4b519f5413d3a68a3fbdaea859fc5d5722d41f7
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-04 15:29:55 +0300

    "Forms added"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import GeneralForm from "./components/GeneralForm";
+import PortalForm from "./components/PortalForm";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<GeneralForm />} />
+        <Route path="/portal" element={<PortalForm />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
