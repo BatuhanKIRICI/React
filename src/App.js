acefe9287700fd8550ce6f4d118756e202d5f8aa
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-08 10:51:29 +0300

    "TypeScript added"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import GeneralForm from "./components/GeneralForm";
-import PortalForm from "./components/PortalForm";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<GeneralForm />} />
-        <Route path="/portal" element={<PortalForm />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
