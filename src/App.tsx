331ffd2d911575ac0a52fc1b97bd5109ad3b7371
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-09 20:34:40 +0300

    Cleanup for MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,8 @@
 import React from "react";
 import "./App.css";
-import Home from "./components/Home";
-import Instructor from "./components/Instructor";
-import InstructorList from "./components/InstructorList";
-import Request from "./components/Request";
-import EventActions from "./components/EventActions";
-import LoginCheck from "./components/LoginCheck";
 
 function App() {
-  const instructor = {
-    firstName: "Miguel",
-    lastName: "Verdugo",
-  };
-
-  const instructorList = [
-    {
-      firstName: "Nathan",
-      lastName: "Mann",
-    },
-    {
-      firstName: "Zachary",
-      lastName: "Kyle",
-    },
-    {
-      firstName: "Alfred",
-      lastName: "O'Neal",
-    },
-  ];
-
-  return (
-    <div className="App">
-      {/* <Home name="Miguel" number={25} isGood={false} />
-      <Instructor instructorInfo={instructor} />
-      <InstructorList instructorList={instructorList} />
-      <Request status="loading" />
-      <EventActions /> */}
-      <LoginCheck />
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
