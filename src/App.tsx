55277df84397db1239c0b1c364216e4d9783230e
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-10 18:30:49 +0300

    "Textfield"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
+import { LessonTextField } from "./components/LessonTextField";
 // import { LessonTypography } from "./components/LessonTypography";
 // import { LessonButton } from "./components/LessonButton";
-import { LessonButtonGroup } from "./components/LessonButtonGroup";
+// import { LessonButtonGroup } from "./components/LessonButtonGroup";
 
 function App() {
   return (
     <div className="App">
       {/* <LessonTypography /> */}
       {/* <LessonButton /> */}
-      <LessonButtonGroup />
+      {/* <LessonButtonGroup /> */}
+      <LessonTextField />
     </div>
   );
 }
