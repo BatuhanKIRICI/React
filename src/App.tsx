24a193a5d4d50398f39ba489b55cd8c73ce26630
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-19 22:54:59 +0300

    "Change value"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
 import { useState } from "react";
 
 const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [workDay, setWorkDay] = useState<number>(0);
 
-  const handleChange = () => {};
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.target.name === "task"
+      ? setTask(e.target.value)
+      : setWorkDay(+e.target.value);
+  };
 
   return (
     <div className="App">
@@ -13,12 +17,14 @@ const App: FC = () => {
         <input
           type="text"
           value={task}
+          name="task"
           placeholder="Enter your task..."
           onChange={handleChange}
         />
         <input
           type="number"
           value={workDay}
+          name="workDay"
           placeholder="How many days to finish?..."
           onChange={handleChange}
         />
