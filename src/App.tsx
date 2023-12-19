e3465739c36481cb025b4b3041cc2cb89ee8c74f
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-19 22:46:23 +0300

    Todo first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
+import { useState } from "react";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [workDay, setWorkDay] = useState<number>(0);
+
+  const handleChange = () => {};
+
+  return (
+    <div className="App">
+      <div>
+        <input
+          type="text"
+          value={task}
+          placeholder="Enter your task..."
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          value={workDay}
+          placeholder="How many days to finish?..."
+          onChange={handleChange}
+        />
+        <button>Add New Task</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
