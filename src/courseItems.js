368f0fc06a8a069d48dd01b4ee5b7b4684364def
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-23 08:36:13 +0300

    "Data"

---
+++ src/courseItems.js
@@ -0,0 +1,32 @@
+const courseItems = [
+  {
+    id: "adsadsadsadsada",
+    title: "sfdbwfrb",
+    price: "69",
+    img: "/assets/cyber-hacker-attack-background-skull-free-vector.jpg",
+    quantity: 1,
+  },
+  {
+    id: "rrberb",
+    title: "yjtkytktyuktuk",
+    price: "69",
+    img: "/assets/cyber-hacker-attack-background-skull-vecdsvsdtor.jpg",
+    quantity: 1,
+  },
+  {
+    id: "gwrgrwebw",
+    title: "etntnrrtnrtn",
+    price: "69",
+    img: "/assets/cyber-hacker-attack-background-skull-vector.jpg",
+    quantity: 1,
+  },
+  {
+    id: "adsadsadsadsadsadsadsaf",
+    title: "rtebbetjttr",
+    price: "69 ",
+    img: "/assets/cyber-hacker-attack-background-skull-xx vector.jpg",
+    quantity: 1,
+  },
+];
+
+export default courseItems;
