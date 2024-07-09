0889ca11c1fa4e1f97ab86eae52df329a6e55a82
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-09 12:55:33 +0200

    "Multiple components"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Heading from "./Heading";
+// import ReactDOM from "react-dom/client";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <Heading firstName="Bob" />
-      <Heading firstName="Tom" />
+    <div>
+      <h1>Task: Add three Card elements</h1>
+      <p>
+        <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3" />
+        <Card h2="Third card's h2" h3="Third card's h3" />
+      </p>
     </div>
   );
 }
