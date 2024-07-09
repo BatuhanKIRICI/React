8eb370c782ab33df0255d70004f1281a16d3d7b4
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-09 11:48:56 +0200

    "Passing props"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Heading from './Heading';
-
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Heading from "./Heading";
 
 function App() {
-
   return (
-    <div>
-      This is the "First Component"
-      <div><Heading /></div>
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="Tom" />
     </div>
   );
 }
