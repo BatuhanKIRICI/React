a30d1bceb6d8439780377db9cd9c6249a58ff01b
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-11 12:00:30 +0200

    "Profile Card"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import "./App.css";
 import React from "react";
-// import ReactDOM from "react-dom/client";
-import Button from "./Button";
 
 function App() {
   return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+      for each web dev skill that you have,
+      customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="./photos/batuhan_kirici.jpg"
+      alt="My Profile"
+    />
+  );
+}
+
+function Intro() {
+  return (
     <div>
-      <Button />
+      <h1>Batuhan Kirici</h1>
+      <p>
+        Frontend Developer. In free times, I like to make sport and listen
+        music. And I do not see reading books as a free-time activity!
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="purple" />
+      <Skill skill="HTML+CSS" emoji="💪" color="green" />
+      <Skill skill="JavaScript" emoji="💪" color="red" />
+      <Skill skill="Git+GitHub" emoji="👍" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      {props.skill}
+      {props.emoji}
     </div>
   );
 }
