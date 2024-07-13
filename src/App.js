b2fe7515279a5837bbee05528a25601c93affa69
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-13 13:25:06 +0200

    "Form Registeration with useState()"

--- src/App.js
+++ src/App.js
@@ -3,17 +3,53 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
-  const [inputText, setText] = useState("hello");
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
+  const [form, setForm] = useState({
+    firstName: "Luke",
+    lastName: "Jones",
+    email: "lukeJones@sculpture.com",
+  });
 
   return (
     <>
-      <input value={inputText} onChange={handleChange} />
-      <p>You typed: {inputText}</p>
-      <button onClick={() => setText("hello")}>Reset</button>
+      <label>
+        First name:
+        <input
+          value={form.firstName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              firstName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={form.lastName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              lastName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          value={form.email}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              email: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <p>
+        {form.firstName} {form.lastName} ({form.email})
+      </p>
     </>
   );
 }
