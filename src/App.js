7c1d9a10f86a4f8062c9d7d85f31d85ba1460aa3
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-13 13:21:50 +0200

    "useState() hook"

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
 import "./App.css";
 import React from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
-      for each web dev skill that you have,
-      customized with props */}
-        <SkillList />
-      </div>
-    </div>
-  );
-}
+  const [inputText, setText] = useState("hello");
 
-function Avatar() {
-  return (
-    <img
-      className="avatar"
-      src="./photos/batuhan_kirici.jpg"
-      alt="My Profile"
-    />
-  );
-}
+  function handleChange(e) {
+    setText(e.target.value);
+  }
 
-function Intro() {
   return (
-    <div>
-      <h1>Batuhan Kirici</h1>
-      <p>
-        Frontend Developer. In free times, I like to make sport and listen
-        music. And I do not see reading books as a free-time activity!
-      </p>
-    </div>
-  );
-}
-
-function SkillList() {
-  return (
-    <div className="skill-list">
-      <Skill skill="React" emoji="💪" color="purple" />
-      <Skill skill="HTML+CSS" emoji="💪" color="green" />
-      <Skill skill="JavaScript" emoji="💪" color="red" />
-      <Skill skill="Git+GitHub" emoji="👍" color="orange" />
-    </div>
-  );
-}
-
-function Skill(props) {
-  return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      {props.skill}
-      {props.emoji}
-    </div>
+    <>
+      <input value={inputText} onChange={handleChange} />
+      <p>You typed: {inputText}</p>
+      <button onClick={() => setText("hello")}>Reset</button>
+    </>
   );
 }
 
