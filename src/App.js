63d40d66ca5fe99b9e927a0c5e35069f5aabd891
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-18 11:03:25 +0200

    "Dsiplaying images"

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import "./App.css";
-import React from "react";
-import { useState } from "react";
+import Logo from "./assets/logo.png";
 
 function App() {
   return (
-    <Main msg="I passed through the Header and the Wrapper and I reached the Button component" />
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={Logo} alt="logo" />
+    </div>
   );
-
-  function Main(props) {
-    return <Header msg={props.msg} />;
-  }
-
-  function Header(props) {
-    return (
-      <div style={{ border: "10px solid whitesmoke" }}>
-        <h1>Header here</h1>
-        <Wrapper msg={props.msg} />
-      </div>
-    );
-  }
-
-  function Wrapper(props) {
-    return (
-      <div style={{ border: "10px solid lightgray" }}>
-        <h2>Wrapper here</h2>
-        <Button msg={props.msg} />
-      </div>
-    );
-  }
-
-  function Button(props) {
-    return (
-      <div style={{ border: "20px solid orange" }}>
-        <h3>This is the Button component</h3>
-        <button onClick={() => alert(props.msg)}>Click me!</button>
-      </div>
-    );
-  }
 }
 
 export default App;
