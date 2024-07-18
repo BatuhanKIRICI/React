f24eef175098af2a5dc2ce625fbd7a62c790366e
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-18 11:18:14 +0200

    "SOng selection"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
 import React from "react";
-import ReactPlayer from "react-player/youtube";
 
-const App = () => {
+function App() {
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  }
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  }
+
   return (
     <div>
-      <MyVideo />
+      <button onClick={toggle1}>Caspian Tern 1</button>
+      <button onClick={toggle2}>Caspian Tern 2</button>
     </div>
   );
-};
-
-const MyVideo = () => {
-  return <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />;
-};
+}
 
 export default App;
