7175a176a8d3b92545fb6e12b2151ce1bcebc447
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-22 12:22:31 +0200

    "useEffect"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [giftCard, setGiftCard] = useState({
-    firstName: "Jennifer",
-    lastName: "Smith",
-    text: "Free dinner for 4 guests",
-    valid: true,
-    instructions: "To use your coupon, click the button below.",
-  });
+  const [toggle, setToggle] = useState(false);
 
-  function spendGiftCard() {
-    setGiftCard((prevState) => {
-      return {
-        ...prevState,
-        text: "Your coupon has been used.",
-        valid: false,
-        instructions: "Please visit our restaurant to renew your gift card.",
-      };
-    });
-  }
+  const handleToggle = () => setToggle(!toggle);
+
+  useEffect(() => {
+    document.title = toggle ? "Welcome to React" : "Using useEffect";
+  }, [toggle]);
 
   return (
     <div style={{ padding: "40px" }}>
-      <h1>Gift Card Page</h1>
-      <h2>
-        Customer: {giftCard.firstName} {giftCard.lastName}
-      </h2>
-      <h3>{giftCard.text}</h3>
-      <p>{giftCard.instructions}</p>
-      {giftCard.valid && (
-        <button onClick={spendGiftCard}>Spend Gift Card</button>
-      )}
+      <h1>Using the useEffect hook</h1>
+      <button onClick={handleToggle}>Toggle</button>
+      {toggle && <h2>Welcome to React</h2>}
     </div>
   );
 }
