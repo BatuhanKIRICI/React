985eeed35bb70a72782050e36200cc8db930eda2
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-22 11:35:52 +0200

    "Managing state within a component"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [greeting, setGreeting] = useState({ greet: "Hello, World" });
-  console.log(greeting, setGreeting);
+  const [giftCard, setGiftCard] = useState({
+    firstName: "Jennifer",
+    lastName: "Smith",
+    text: "Free dinner for 4 guests",
+    valid: true,
+    instructions: "To use your coupon, click the button below.",
+  });
 
-  function updateGreeting() {
-    const newGreeting = { ...greeting };
-    newGreeting.greet = "Hello, World-Wide Web";
-    setGreeting(newGreeting);
+  function spendGiftCard() {
+    setGiftCard((prevState) => {
+      return {
+        ...prevState,
+        text: "Your coupon has been used.",
+        valid: false,
+        instructions: "Please visit our restaurant to renew your gift card.",
+      };
+    });
   }
 
   return (
-    <div>
-      <h1>{greeting.greet}</h1>
-      <button onClick={updateGreeting}>Update greeting</button>
+    <div style={{ padding: "40px" }}>
+      <h1>Gift Card Page</h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>{giftCard.text}</h3>
+      <p>{giftCard.instructions}</p>
+      {giftCard.valid && (
+        <button onClick={spendGiftCard}>Spend Gift Card</button>
+      )}
     </div>
   );
 }
