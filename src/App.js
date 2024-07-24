29f0685807d2626267cc8171296da904b58a7b2d
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-24 13:53:19 +0200

    "Fetching data"

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [btcData, setBtcData] = useState({});
+  const [user, setUser] = useState([]);
 
   const fetchData = () => {
-    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
-      .then((response) => response.json())
-      .then((jsonData) => setBtcData(jsonData.bpi.USD))
-      .catch((error) => console.log(error));
+    fetch("https://randomuser.me/api/?results=1").then((response) =>
+      response.json().then((data) => setUser(data))
+    );
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <>
-      <h1>Current BTC/USD data</h1>
-      <p>Code: {btcData.code}</p>
-      <p>Symbol: {btcData.symbol}</p>
-      <p>Rate: {btcData.rate}</p>
-      <p>Description: {btcData.description}</p>
-      <p>Rate Float: {btcData.rate_float}</p>
-    </>
+  return Object.keys(user).length > 0 ? (
+    <div style={{ padding: "40px" }}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="" />
+    </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
