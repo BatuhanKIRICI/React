5e917dda56661fabf67c1e22e250c646cbe26bcf
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-10 12:03:25 +0200

    "Flashcards"

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
+import { useState } from "react";
 import "./App.css";
-import { useEffect, useState } from "react";
 
-const MousePosition = ({ render }) => {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  useEffect(() => {
-    const handleMousePositionChange = (e) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    };
-
-    window.addEventListener("mousemove", handleMousePositionChange);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMousePositionChange);
-    };
-  }, []);
-
-  return render({ mousePosition });
-};
-
-const PanelMouseLogger = () => {
-  return (
-    <div className="BasicTracker">
-      <p>Mouse position:</p>
-      <MousePosition
-        render={({ mousePosition }) => (
-          <div className="Row">
-            <span>x: {mousePosition.x}</span>
-            <span>y: {mousePosition.y}</span>
-          </div>
-        )}
-      />
-    </div>
-  );
-};
-
-const PointMouseLogger = () => {
-  return (
-    <MousePosition
-      render={({ mousePosition }) => (
-        <p>
-          ({mousePosition.x}, {mousePosition.y})
-        </p>
-      )}
-    />
-  );
-};
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="Header">Little Lemon Restaurant 🍕</header>
-      <PanelMouseLogger />
-      <PointMouseLogger />
+      <FlashCards />
+    </div>
+  );
+}
+
+function FlashCards() {
+  const [selectedID, setSelectedID] = useState(null);
+
+  const handleClick = (id) => setSelectedID(id !== selectedID ? id : null);
+
+  return (
+    <div className="flashcards">
+      {questions.map((item) => (
+        <div
+          key={item.id}
+          className={item.id === selectedID ? "selected" : ""}
+          onClick={() => handleClick(item.id)}
+        >
+          <p>{item.id === selectedID ? item.answer : item.question}</p>
+        </div>
+      ))}
     </div>
   );
 }
